e846a0d50e630ceb5d5b11812660e9328493d00b
Cait :) <99435089+mobleyc2@users.noreply.github.com>
2022-04-19 20:28:00 -0400

    Add files via upload

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math (Cow, Otter, Frog) {
+    return (Cow + (Otter * Frog));
+}
+console.log(math(53, 61, 67));
---
+++ roundingnumbers.js
@@ -0,0 +1,4 @@
+let roundup = 1.5
+Math.round(1.5)
+let rounded = roundup
+console.log(2.0)
